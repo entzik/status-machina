d1011327bdb31b038a411a88ed38720d74bc1858
emil.kirschner <emil@liquidshare.io>
2019-10-11 16:19:10 +0200

    publish packages

--- status-machina-core/build.gradle.kts
+++ status-machina-core/build.gradle.kts
@@ -9,6 +9,7 @@
 plugins {
     // Apply the java-library plugin to add support for Java Library
     `java-library`
+    `maven-publish`
     `idea`
     `eclipse`
     id("org.springframework.boot") version "2.1.6.RELEASE"
@@ -40,6 +41,16 @@ tasks.getByName("jar") {
     enabled = true
 }
 
+tasks.register<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allJava)
+}
+
+tasks.register<Jar>("javadocJar") {
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc.get().destinationDir)
+}
+
 tasks.getByName("bootJar") {
     enabled = false
 }
@@ -51,3 +62,23 @@ tasks.test {
     }
 }
 
+publishing {
+    repositories {
+        maven {
+            // change to point to your repo, e.g. http://my.org/repo
+            name = "GitHub"
+            url = uri("https://maven.pkg.github.com/entzik/status-machina")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GPR_USER")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GPR_API_KEY")
+            }
+        }
+    }
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+        }
+    }
+}
